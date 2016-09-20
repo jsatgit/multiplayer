b404a087adcabab927276796e8cfaf8d2dea1bb6
James Shi <jshi@live.ca>
2016-09-19 23:11:11 -0700

    people and houses with bugs and bad code quality

--- app.py
+++ app.py
@@ -1,24 +1,45 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO
-from flask_socketio import send, emit
+from flask_socketio import SocketIO, send, emit
+from time import sleep
+from threading import Thread
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
-class Counter:
+def send(lat, lng):
+    print 'sending'
+    emit('marker', {'lat': lat, 'lng': lng}, broadcast=True, include_self=False)
+    sleep(5)
+
+class Game:
     def __init__(self):
-        self.count = 0
+        self.ready = False
 
-    def update(self):
-        self.count += 1
+    def start(self):
+        send(3, 10)
+        send(43, 20)
+        send(-33, -110)
 
-counter = Counter()
+    def ready(self):
+        self.ready = True
 
-@socketio.on('update')
-def on_update():
-    counter.update() 
-    emit('count', counter.count, broadcast=True)
+game = Game()
+
+def sendToEveryone(event, data):
+    emit(event, data, broadcast=True, include_self=False)
+
+@socketio.on('addMarker')
+def on_addMarker(event):
+    print event['type']
+    print event['latlong']
+    sendToEveryone('markerReceived', event);
+
+@socketio.on('setPosition')
+def on_setPosition(event):
+    print event['person']
+    print event['loc']
+    sendToEveryone('positionSetReceived', event);
 
 if __name__ == '__main__':
     socketio.run(app)
--- game.js
+++ game.js
@@ -1,49 +1,164 @@
-var ClickerView = function() {
-  this.clicker = document.getElementById('clicker');
-};
+var map;
+var socket;
+var coquitlam = {lat: 49.2838, lng: -122.7932};
+var currentLoc = coquitlam;
+var LEFT = 97;
+var RIGHT = 100;
+var UP = 119;
+var DOWN = 115;
+var INCREMENT = 0.00001;
+var SPACE = 32;
+var currentMarker;
+var personIcon = 'https://www.apec-econ.ca/system/style/images/icon-small-person.png'
+var houseIcon = 'http://findicons.com/files/icons/1672/mono/32/home.png'
 
-ClickerView.prototype.onClick = function(callback) {
-  this.clicker.onclick = callback;
-};
-
-var CounterView = function() {
-  this.counter = document.getElementById('count');
-};
-
-CounterView.prototype.show = function(count) {
-  this.counter.innerHTML = count;
-};
-
-var Server = function() {
-  var domain = 'localhost';
-  var port = '5000';
-  this.socket = io.connect('http://' + domain + ':' + port);
-  this.socket.on('connect', function() {
-    console.log('Connected to server');
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: currentLoc,
+    zoom: 18 
   });
-  var self = this;
-  this.socket.on('count', function(count) {
-    self.callback(count);
+  onMapLoaded();
+}
+
+function up(loc) {
+  return {
+    lat: loc.lat + INCREMENT,
+    lng: loc.lng
+  };
+}
+
+function down(loc) {
+  return {
+    lat: loc.lat - INCREMENT,
+    lng: loc.lng
+  };
+}
+
+function left(loc) {
+  return {
+    lat: loc.lat,
+    lng: loc.lng - INCREMENT 
+  };
+}
+
+function right(loc) {
+  return {
+    lat: loc.lat,
+    lng: loc.lng + INCREMENT 
+  };
+}
+
+document.addEventListener('keypress', function(evt) {
+  console.log(evt.which);
+  switch (evt.which) {
+    case UP:
+      currentLoc = up(currentLoc);
+      setPos(currentLoc);
+      break;
+    case DOWN:
+      currentLoc = down(currentLoc);
+      setPos(currentLoc);
+      break;
+    case LEFT:
+      currentLoc = left(currentLoc);
+      setPos(currentLoc);
+      break;
+    case RIGHT:
+      currentLoc = right(currentLoc);
+      setPos(currentLoc);
+      break;
+    case SPACE:
+      addHouse(currentLoc);
+      break;
+  }
+});
+
+function panToMarker() {
+  panTo(currentMarker)
+}
+
+function setPos(loc) {
+  serverSetPos(loc);
+  currentMarker.setPosition(loc);
+}
+
+function serverSetPos(loc) {
+  socket.emit('setPosition', {
+    person: 'me',
+    loc: loc 
   });
 }
 
-Server.prototype.update = function() {
-  this.socket.emit('update');
-};
+function panTo(marker) {
+  map.panTo(marker.getPosition());
+}
 
-Server.prototype.onUpdate = function(callback) {
-  this.callback = callback
-};
+function addPerson(latlong) {
+  addMarkerToServer(latlong, 'person');
+  return addMarker(latlong, personIcon);
+}
 
+function addHouse(latlong) {
+  addMarkerToServer(latlong, 'house');
+  return addMarker(latlong, houseIcon);
+}
 
-var clickerView = new ClickerView();
-var counterView = new CounterView();
-var server = new Server();
+function addMarker(latlong, icon) {
+  return marker = new google.maps.Marker({
+    position: latlong,
+    map: map,
+    title: 'Hello World!',
+    icon: icon
+  });
+}
 
-server.onUpdate(function(count) {
-  counterView.show(count);
-});
+function addMarkerToServer(latlong, type) {
+  socket.emit('addMarker', {
+    type: type,
+    latlong: latlong
+  })
+}
 
-clickerView.onClick(function() {
-  server.update();
-});
+var isConnected = false;
+var isMapLoaded = false;
+
+function onConnected() {
+  isConnected = true;
+  if (isMapLoaded) {
+    onReady();
+  }
+}
+
+function onMapLoaded() {
+  isMapLoaded = true;
+  if (isConnected) {
+    onReady();
+  }
+}
+
+function onReady() {
+  currentMarker = addPerson(currentLoc);
+}
+
+var otherPerson;
+
+function connectToServer() {
+   var domain = 'localhost';
+   var port = '5000';
+   socket = io.connect('http://' + domain + ':' + port);
+   socket.on('connect', function() {
+     onConnected();
+   });
+   socket.on('markerReceived', function(evt) {
+     if (evt.type == 'person') {
+       otherPerson = addMarker(evt.latlong, personIcon);
+     } else if (evt.type == 'house') {
+       addMarker(evt.latlong, houseIcon);
+     }
+   });
+   socket.on('positionSetReceived', function(evt) {
+     otherPerson.setPosition(evt.loc)
+   });
+}
+
+connectToServer();
